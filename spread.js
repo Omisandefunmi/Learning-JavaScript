3bd5ec180e5bfc5e6578799c4f059e78a4a05f3d
Omisandefunmi <omisandefunmi@gmail.com>
2022-06-18 22:41:40 +0100

    added array.js and spread.js file

---
+++ spread.js
@@ -0,0 +1,7 @@
+let classMembers = ["tolani", "bunmi", "adewale", "bassey", "dolapo"]
+
+let restMembers = ["ebube", "funmi", "yemisi", "ernest", "segun"]
+
+let everyMember = [...classMembers, ...restMembers]
+
+console.log(everyMember)
